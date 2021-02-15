8114b656cc9114febb007fbc204a5043507ef7da
Ryan Tremarco <ryantremarco@hotmail.co.uk>
2021-02-15 22:35:49 +0000

    Removed default React App files / content ready to build on. Also snapshot testing now because why not I guess.

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,7 @@
-import { render, screen } from '@testing-library/react';
+import renderer from 'react-test-renderer';
 import App from './App';
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const tree = renderer.create(<App/>).toJSON();
+  expect(tree).toMatchSnapshot();
 });
