cf0184e7c1caf3ad0a36c5940e7ab26e7220121e
Ryan Tremarco <ryantremarco@hotmail.co.uk>
2021-02-16 09:47:54 +0000

    Added bulma, and created initial (empty) homepage to check styling

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
+import React from 'react';
+import Home from "./pages/home";
+
 const App = () => {
   return (
-    <div>
-      <h1>
-        Ryan Tremarco
-      </h1>
-      <p>
-        Eventually this will have information but for now, enjoy this text with zero styling.
-      </p>
-    </div>
+    <Home/>
   );
 }
 
---
+++ src/pages/home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => (
+  <>
+    <h1 className="title">Ryan Tremarco</h1>
+    <p className="subtitle">
+      Content incoming
+    </p>
+  </>
+)
+
+export default Home;
