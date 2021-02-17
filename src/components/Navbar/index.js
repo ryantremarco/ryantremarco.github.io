a98451d23d5db321a0ca895d0a7acbdfb0632fe6
Ryan Tremarco <ryantremarco@hotmail.co.uk>
2021-02-17 19:38:06 +0000

    Added burger to the navbar for mobile

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,34 +1,61 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const Navbar = () => (
-  <nav className="navbar">
+const Navbar = () => {
+
+  const [burgerActive, setBugerActive] = React.useState(false);
+
+  const NavbarLink = ({to, button=false, children}) => (
+    <Link 
+      className={`navbar-item ${button ? 'button is-primary' : ''}`}
+      to={to}
+      // Bulma normally handles this, but not the link resolves to the current component (/, /home)
+      onClick={() => setBugerActive(false)}
+    >
+      {children}
+    </Link>
+  )
+
+  return <nav className="navbar">
     <div className="navbar-brand">
       <Link className="navbar-item" to="/">
         <h1 className="title">Ryan Tremarco</h1>
       </Link>
+      <a 
+        class={`navbar-burger burger ${burgerActive? 'is-active' : ''}`}
+        data-target="navbar-content"
+        onClick={() => setBugerActive(!burgerActive)}
+      >
+        <span></span>
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+      </a>
     </div>
-    <div className="navbar-start">
-      <Link className="navbar-item" to="/">Home</Link>
-      <Link className="navbar-item" to="/about">About Me</Link>
-      <Link className="navbar-item" to="/dev">Development</Link>
-      <Link className="navbar-item" to="/misc">Misc</Link>
-    </div>
-    <div className="navbar-end">
-      <div class="navbar-item">
-        <div class="field is-grouped">
-          <p class="control">
-            <Link class="button is-primary" to="/contact">
-              <span class="icon">
-                <i class="fas fa-envelope"></i>
-              </span>
-              <span>Contact</span>
-            </Link>
-          </p>
+    <div id="navbar-content" className={`navbar-menu ${burgerActive? 'is-active' : ''}`}>
+      <div className="navbar-start">
+        <NavbarLink to="/">Home</NavbarLink>
+        <NavbarLink to="/about">About Me</NavbarLink>
+        <NavbarLink to="/dev">Development</NavbarLink>
+        <NavbarLink to="/misc">Misc</NavbarLink>
+      </div>
+      <div className="navbar-end">
+        <div class="navbar-item">
+          <div class="field is-grouped">
+            <p class="control">
+              <NavbarLink button to="/contact">
+                <span class="icon">
+                  <i class="fas fa-envelope"></i>
+                </span>
+                <span>Contact</span>
+              </NavbarLink>
+            </p>
+          </div>
         </div>
       </div>
     </div>
   </nav>
-);
+};
 
 export default Navbar;
