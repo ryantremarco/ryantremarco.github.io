196db1584253539e6d8aec731ec4ad7b51593b61
Ryan Tremarco <ryantremarco@hotmail.co.uk>
2021-02-20 19:07:49 +0000

    Added initial info for Experience section

--- src/sections/Contact/index.js
+++ src/sections/Contact/index.js
@@ -2,7 +2,7 @@ import PageSection from "../../components/PageSection";
 
 const Contact = () => (
   <PageSection title="Contact" id="contact">
-    <div className="column is-half has-text-centered">
+    <div className="column is-6 has-text-centered">
       <p className="has-text-centered">
         I'm currently looking for exciting  work opportunities in Japan.<br/>
         If you think you have something I may be interested in (or you just want to say hi) please don't hesitate to send me a message.
--- src/sections/Experience/index.js
+++ src/sections/Experience/index.js
@@ -1,8 +1,54 @@
 import PageSection from "../../components/PageSection";
 
+const ExperiencePane = ({ company, date, children }) => (
+  <div className="columns experience-pane">
+    <div className="column is-2">
+      <h2 className="title is-size-4 has-text-right-tablet">{company}</h2>
+      <p className="subtitle has-text-right-tablet">{date}</p>
+    </div>
+    <div className="column is-9 content">
+      {children}
+    </div>
+  </div>
+); 
+
 const Experience = () => (
   <PageSection title="Experience" id="experience">
-    Experience section coming soon
+    <div className="column is-8">
+      <ExperiencePane company="HMG" date="2017 - ?">
+        <ul>
+          <li>
+            Full-stack web development primarily using Java|SpringBoot
+            and JavaScript|React to create apps for internal customers.
+          </li>
+          <li>
+            Implementing good DevOps practices using OpenShift, AWS, and 
+            Jenkins.
+          </li>
+          <li>
+            Leading internal multiple product development teams, in both 
+            technical and Agile capacities.
+          </li>
+          <li>
+            Working as part of a dedicated innovation team, tasked with 
+            investigating potential application of new technologies to business 
+            benefit.
+          </li>
+        </ul>
+      </ExperiencePane>
+      <ExperiencePane company="VLDB" date="2015">
+        <ul>
+          <li>
+            Designed and developed a cross-platform Xamarin mobile app for an 
+            internal sales team to use with customers.
+          </li>
+          <li>
+            Wrote automation scripts in Visual Basic for extracting, processing, 
+            and revisualising customer data.
+          </li>
+        </ul>
+      </ExperiencePane>
+    </div>
   </PageSection>
 )
 
