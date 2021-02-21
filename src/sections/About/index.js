79ca5f532313b73eeb45a72dc87863b3600c86b7
Ryan Tremarco <ryantremarco@hotmail.co.uk>
2021-02-21 09:59:47 +0000

    Tweaked some content wording

--- src/sections/About/index.js
+++ src/sections/About/index.js
@@ -5,13 +5,16 @@ const About = () => (
     <div className="column is-4">
       <h2 className="title is-size-4">Me</h2>
       <p>
-        Hello! I'm Ryan, a full-stack Software Eningeer working for Her Majesty's Governement in the UK.
+        Hello! I'm Ryan, a full-stack Software Eningeer working for Her Majesty's Government 
+        in the UK.
       </p>
       <p>
-        Since completing a government apprenticeship scheme and obtaining my first-class honours degree, I've primarily been working on everything web application.
+        Since completing a government apprenticeship scheme and obtaining my first-class 
+        honours degree, I've primarily been working on everything web application.
       </p>
       <p>
-        I am currently looking to progress my career further down into the tech stack - be it exlusively web backend, or systems development.
+        I am currently looking to progress my career further down into the tech stack 
+        - be it exlusively web backend, or systems development.
       </p>
     </div>
 
@@ -20,13 +23,17 @@ const About = () => (
     <div className="column is-4">
       <h2 className="title is-size-4">My Interests</h2>
       <p>
-        Recently, I have been spending a lot of my free time learning Japanese since I plan to live there soon. I still have a ways to go, but it's a fun journey.
+        Because I intend to move to Japan soon, I have recently been spending a lot of 
+        my free time learning Japanese. I still have a ways to go, but it's a fun journey.
       </p>
       <p>
-        I enjoy playing around with technologies I don't get to use day-to-day ie. Rust, Ruby, and lately even some Haskell. This extends to pretending I'm a game developer every now and again.
+        I enjoy playing around with technologies I don't get to use day-to-day ie. Rust, 
+        Ruby, and lately even some Haskell. This also extends to pretending that I'm a game 
+        developer every now and again.
       </p>
       <p>
-        Taking part in table-top roleplaying games is another hobby of mine, but I tend to roll a lot of 1s.
+        Taking part in table-top roleplaying games is another hobby of mine, but I tend to 
+        roll a lot of 1s.
       </p>
     </div>
   </PageSection>
--- src/sections/Contact/index.js
+++ src/sections/Contact/index.js
@@ -5,7 +5,8 @@ const Contact = () => (
     <div className="column is-6 has-text-centered">
       <p className="has-text-centered">
         I'm currently looking for exciting  work opportunities in Japan.<br/>
-        If you think you have something I may be interested in (or you just want to say hi) please don't hesitate to send me a message.
+        If you think you have something I may be interested in (or you just want to 
+        say hi) please don't hesitate to send me a message.
       </p>
       <span className="contact-button">
         <a
