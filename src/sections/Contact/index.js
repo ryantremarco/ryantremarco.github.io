0535b08c98bbee9b09a6bb831db8b15278c2fc8a
Ryan Tremarco <ryantremarco@hotmail.co.uk>
2022-09-01 22:05:55 +0100

    updated cv and contact text

--- src/sections/Contact/index.js
+++ src/sections/Contact/index.js
@@ -4,8 +4,8 @@ const Contact = () => (
   <PageSection title="Contact" id="contact">
     <div className="column is-6 has-text-centered">
       <p className="has-text-centered">
-        I'm currently looking for exciting  work opportunities in Japan.<br/>
-        If you think you have something I may be interested in (or you just want to 
+        I'm not actively looking for new opportunities right now, but
+        if you think you have something I may be interested in (or you just want to 
         say hi) please don't hesitate to send me a message.
       </p>
       <section className="section">
